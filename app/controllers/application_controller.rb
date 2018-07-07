9e8766a67d0ea2fcc11b7cd8dd6da19e00d60b6a
T.C <travis18@gmail.com>
2018-07-08 04:17:42 +0800

    enable devise on backend pages, and only admin role can view wzht pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+
+  private
+
+  def authenticate_admin
+    unless current_user.admin?
+      flash[:alert] = "You are not admin. Not allow!"
+      redirect_to root_path
+    end
+  end
 end
