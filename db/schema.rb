c516681aa904eebcfe00332b3d4aab9b97070ac0
T.C <travis18@gmail.com>
2018-07-09 02:09:49 +0800

    add column "image" on restaurant table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_07_150637) do
+ActiveRecord::Schema.define(version: 2018_07_08_180748) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2018_07_07_150637) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image"
   end
 
   create_table "users", force: :cascade do |t|
