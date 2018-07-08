6acec4f08af21994f06fb68e90fdc13b37998df3
T.C <travis18@gmail.com>
2018-07-08 15:22:44 +0800

    implement backend's create restaurant feature

--- app/controllers/wzht/restaurants_controller.rb
+++ app/controllers/wzht/restaurants_controller.rb
@@ -4,4 +4,25 @@ class Wzht::RestaurantsController < ApplicationController
   def index
     @restaurants = Restaurant.all
   end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      flash[:notice] = "restaurant was successfully created"
+      redirect_to wzht_restaurants_path
+    else
+      flash.now[:alert] = "restaurant was failed to created"
+      render :new
+    end
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :opening_hours, :tel, :address, :description)
+  end
 end
