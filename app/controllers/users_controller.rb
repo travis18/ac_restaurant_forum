6fefd3c861098ac30beaaab2e2c33710dcd4fef9
T.C <travis18@gmail.com>
2018-07-23 20:47:03 +0800

    add profile edit function fix routes, add update action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,9 +4,22 @@ class UsersController < ApplicationController
     @user = current_user
   end
 
+  def edit
+    @user = current_user
+  end
+
+  def update
+    if current_user.update(profile_params)
+      flash.notice = "Your profile has been updated successfully"
+      redirect_to user_path
+    else
+      flash.alert = "There is something wrong in your profile"
+    end
+  end
+
   private
 
   def profile_params
-    params.require(:user).permit(:name, :intro, :avartar)
+    params.require(:user).permit(:name, :intro, :avatar)
   end
 end
