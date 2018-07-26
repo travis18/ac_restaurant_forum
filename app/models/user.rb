7fcd97c186ed9eded1e18ebb78f33b02364b9fab
T.C <travis18@gmail.com>
2018-07-26 15:17:26 +0800

    add dependency between models

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  has_many :comments
+  has_many :comments, dependent: :restrict_with_error
   has_many :restaurants, through: :comments
   mount_uploader :avatar, AvatarUploader
 
