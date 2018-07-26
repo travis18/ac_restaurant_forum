ff7c7c6ef659f5590b66b3e598ba50ef99f9fe4e
T.C <travis18@gmail.com>
2018-07-26 15:12:03 +0800

    integrate feeds page to root

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -3,10 +3,18 @@ class RestaurantsController < ApplicationController
   def index
     @restaurants = Restaurant.page(params[:page]).per(9)
     @categories = Category.all
+    @nav = "index"
   end
 
   def show
     @restaurant = Restaurant.find(params[:id])
     @comment = Comment.new
   end
+
+  def feeds
+    @recent_restaurants = Restaurant.order(created_at: :desc).limit(10)
+    @recent_comments = Comment.order(created_at: :desc).limit(10)
+    @nav = "feeds"
+  end
+
 end
