ceeb12f58051eabf373b87b739b2ab8cc7ed7f00
T.C <travis18@gmail.com>
2018-07-29 18:10:16 +0800

    add users index route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :users, only: [:show, :edit, :update]
+  resources :users, only: [:show, :edit, :update, :index]
 
   resources :followships, only: [:create, :destroy]
 
